3f020fb73d6ded1b465d0209807afb67f594c8bb
Hieuvtlt <dinhhieuvtlt@gmail.com>
2026-08-10 23:08:07 +0700

    Fix missing screen: add LuyenKhiScreen

---
+++ src/ui/screens/LuyenKhiScreen.js
@@ -0,0 +1,11 @@
+export function LuyenKhiScreen() {
+  return `
+    <div class="profession-screen">
+      <h3 class="panel-title-sm">Luyện khí</h3>
+      <div class="profession-layout">
+        <section class="profession-panel"><h4>Loại khí</h4><div class="profession-item is-selected" data-col="khi" data-rank="chon">Trang bị cơ bản</div></section>
+        <section class="profession-panel"><h4>Phẩm cấp</h4><div class="profession-rank is-selected" data-col="khi" data-rank="ha">Hạ phẩm</div><div class="profession-rank" data-col="khi" data-rank="trung">Trung phẩm</div><div class="profession-rank" data-col="khi" data-rank="thuong">Thượng phẩm</div><div class="profession-rank" data-col="khi" data-rank="cuc">Cực phẩm</div></section>
+        <section class="profession-panel"><h4>Thông tin</h4><div class="product-line">Tên: <b>Khí</b></div><div class="product-line">Phẩm cấp: <b>Hạ phẩm</b></div><div class="product-line">Yêu cầu: <b>-</b></div><div class="product-line">Mô tả: <b>Chưa có dữ liệu</b></div></section>
+      </div>
+    </div>`
+}
