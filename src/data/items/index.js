2435aedd4d7ce0a4e69cd45474e382f48e216add
Hieuvtlt <dinhhieuvtlt@gmail.com>
2026-08-11 09:39:00 +0700

    Mở rộng kho item với đan dược, linh dược, quặng và bí kíp

--- src/data/items/index.js
+++ src/data/items/index.js
@@ -2,12 +2,18 @@ import { weapons } from './weapons.js'
 import { armors } from './armor.js'
 import { consumables } from './consumables.js'
 import { materials } from './materials.js'
+import { alchemyMaterials } from './alchemy.js'
+import { forgingMaterials } from './forging.js'
+import { manuals } from './manuals.js'
 
 export const items = [
   ...weapons,
   ...armors,
   ...consumables,
   ...materials,
+  ...alchemyMaterials,
+  ...forgingMaterials,
+  ...manuals,
 ]
 
 export const itemById = new Map(items.map((item) => [item.id, item]))
