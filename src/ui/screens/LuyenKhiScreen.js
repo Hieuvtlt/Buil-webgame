5c5dc3b1cf76def9ca838e8332657ee1525721bd
Hieuvtlt <dinhhieuvtlt@gmail.com>
2026-08-14 09:45:03 +0700

    Fix GitHub Pages asset path for hammer anvil

--- src/ui/screens/LuyenKhiScreen.js
+++ src/ui/screens/LuyenKhiScreen.js
@@ -1,12 +1,14 @@
 import '../../craft.css'
 import { mountLuyenKhi } from '../controllers/CraftController.js'
 
+const assetBase = import.meta.env.BASE_URL
+
 export function LuyenKhiScreen() {
   const html = `
     <div class="craft-screen game-screen craft-forging">
       <div class="craft-layout">
         <section class="craft-main-panel">
-          <div class="craft-machine-frame"><img class="craft-machine-art craft-machine-art-forge" src="/assets/craft/hammer-anvil.svg" alt="Búa và đe luyện khí"></div>
+          <div class="craft-machine-frame"><img class="craft-machine-art craft-machine-art-forge" src="${assetBase}assets/craft/hammer-anvil.svg" alt="Búa và đe luyện khí"></div>
           <div class="craft-fields">
             <label class="craft-field"><span>Loại trang bị</span><select data-craft-type><option value="">-- Chọn loại trang bị --</option><option value="vu_khi">Vũ khí</option><option value="mu">Mũ</option><option value="ao">Áo</option><option value="baotay">Bao tay</option><option value="dailung">Đai lưng</option><option value="giay">Giày</option><option value="daychuyen">Dây chuyền</option><option value="ngocboi">Ngọc bội</option><option value="nhan">Nhẫn</option></select></label>
             <label class="craft-field"><span>Level</span><select data-craft-level><option value="">-- Chọn level --</option>${Array.from({ length: 120 }, (_, i) => `<option value="${i + 1}">Trang bị Lv${i + 1}</option>`).join('')}</select></label>
