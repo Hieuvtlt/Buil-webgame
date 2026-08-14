5af62f682ea191b6e1c7363698c6d36a81179538
Hieuvtlt <dinhhieuvtlt@gmail.com>
2026-08-14 11:20:45 +0700

    Sửa Luyện Đan dùng asset hinhdanlo tập trung

--- src/ui/screens/LuyenDanScreen.js
+++ src/ui/screens/LuyenDanScreen.js
@@ -8,7 +8,7 @@ export function LuyenDanScreen() {
     <div class="craft-screen game-screen craft-alchemy">
       <div class="craft-layout">
         <section class="craft-main-panel">
-          <div class="craft-machine-frame"><img class="craft-machine-art craft-machine-art-alchemy" src="${assetBase}assets/craft/dan-lo-user.jpg" alt="Đan lô"></div>
+          <div class="craft-machine-frame"><img class="craft-machine-art craft-machine-art-alchemy" src="${assetBase}assets/images/hinhdanlo.svg" alt="Đan lô"></div>
           <div class="craft-fields">
             <label class="craft-field"><span>Loại đan</span><select data-craft-type><option value="">-- Chọn loại đan --</option><option value="hoimau">Hồi Khí Đan</option><option value="hoimana">Hồi Nguyên Đan</option><option value="exp">Tụ Linh Đan</option><option value="expskill">Ngộ Đạo Đan</option></select></label>
             <label class="craft-field"><span>Level</span><select data-craft-level><option value="">-- Chọn level --</option>${Array.from({ length: 10 }, (_, i) => `<option value="${i + 1}">Đan dược Lv${i + 1}</option>`).join('')}</select></label>
