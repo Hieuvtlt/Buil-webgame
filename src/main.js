dd2843c98471492c434a0bf049fe837178b2a7c4
Hieuvtlt <dinhhieuvtlt@gmail.com>
2026-08-14 17:49:10 +0700

    Connect quest menu controller

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ import { LuyenDanScreen } from './ui/screens/LuyenDanScreen.js'
 import { LuyenKhiScreen } from './ui/screens/LuyenKhiScreen.js'
 import { HopThanhScreen, mountHopThanhScreen } from './ui/screens/HopThanhScreen.js'
 import { ThuongHoiScreen } from './ui/screens/ThuongHoiScreen.js'
-import { NhiemVuScreen } from './ui/screens/NhiemVuScreen.js'
+import { NhiemVuScreen, mountNhiemVuScreen } from './ui/screens/NhiemVuScreen.js'
 import { NgoaiCanhScreen } from './ui/screens/NgoaiCanhScreen.js'
 import { GMScreen } from './ui/screens/GMScreen.js'
 import { SettingsScreen } from './ui/screens/SettingsScreen.js'
@@ -36,7 +36,7 @@ const screens = {
   'Luyện khí': { render: LuyenKhiScreen },
   'Hợp thành': { render: HopThanhScreen, mount: mountHopThanhScreen },
   'Thương hội': { render: ThuongHoiScreen, mount: mountMerchantScreen },
-  'Nhiệm vụ': { render: NhiemVuScreen },
+  'Nhiệm vụ': { render: NhiemVuScreen, mount: mountNhiemVuScreen },
   'Ngoại cảnh': { render: NgoaiCanhScreen },
   'GM': { render: GMScreen },
   'Cài đặt': { render: SettingsScreen },
