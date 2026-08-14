72d8d2fe35b358169023db52e86b53ad9a17057e
Hieuvtlt <dinhhieuvtlt@gmail.com>
2026-08-14 11:20:57 +0700

    Sửa Luyện Khí dùng asset hinhbualuyenkhi tập trung

--- src/ui/screens/LuyenKhiScreen.js
+++ src/ui/screens/LuyenKhiScreen.js
@@ -8,7 +8,7 @@ export function LuyenKhiScreen() {
     <div class="craft-screen game-screen craft-forging">
       <div class="craft-layout">
         <section class="craft-main-panel">
-          <div class="craft-machine-frame"><img class="craft-machine-art craft-machine-art-forge" src="${assetBase}assets/craft/luyen-khi-user.jpg" alt="Búa và đe luyện khí"></div>
+          <div class="craft-machine-frame"><img class="craft-machine-art craft-machine-art-forge" src="${assetBase}assets/images/hinhbualuyenkhi.svg" alt="Búa và đe luyện khí"></div>
           <div class="craft-fields">
             <label class="craft-field"><span>Loại trang bị</span><select data-craft-type><option value="">-- Chọn loại trang bị --</option><option value="vu_khi">Vũ khí</option><option value="mu">Mũ</option><option value="ao">Áo</option><option value="baotay">Bao tay</option><option value="dailung">Đai lưng</option><option value="giay">Giày</option><option value="daychuyen">Dây chuyền</option><option value="ngocboi">Ngọc bội</option><option value="nhan">Nhẫn</option></select></label>
             <label class="craft-field"><span>Level</span><select data-craft-level><option value="">-- Chọn level --</option>${Array.from({ length: 120 }, (_, i) => `<option value="${i + 1}">Trang bị Lv${i + 1}</option>`).join('')}</select></label>
